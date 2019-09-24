e864dec6820f939d11cd4dddb0fd3ba469163917
gangsoogi <50457823+gangsoogi@users.noreply.github.com>
2019-09-24 13:55:31 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
+PImage gnimg;
+
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(255,0,0);
+  size(1227, 816);
+  //set size according to background image size
+  gnimg = loadImage("gnimg.JPG");
+  background(gnimg);
+
+  //loading background of the app
 }
-    
+
+
 function draw() {
-    if (mouseIsPressed) {
-        var size = random(10, 100);
-        ellipse(mouseX, mouseY, size, size);
+  for (int i = 0; i < 1227; i = i+10) {
+    for (int a = 0; a < 816; a = a+10) {
+      color c = get(i, a);
+      fill(c);
+      noStroke();
+      rect(i, a, 10, 10);
     }
+  }
 }
