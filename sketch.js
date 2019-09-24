8e71a489ca809832e90e7470469f5818d25dca9d
gangsoogi <50457823+gangsoogi@users.noreply.github.com>
2019-09-24 14:17:43 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-PImage gnimg;
-
+let gnimg;
+function preload() {
+  gnimg = loadImage('creative_computing/gnimg.JPG');
+}
 
 function setup() {
   size(1227, 816);
@@ -11,10 +13,12 @@ function setup() {
 }
 
 
+
+
 function draw() {
-  for (int i = 0; i < 1227; i = i+10) {
-    for (int a = 0; a < 816; a = a+10) {
-      color c = get(i, a);
+  for (let i = 0; i < 1227; i = i+10) {
+    for (let a = 0; a < 816; a = a+10) {
+      let c = get(i, a);
       fill(c);
       noStroke();
       rect(i, a, 10, 10);
